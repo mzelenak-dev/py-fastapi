eacd4dd2936fa0e7c9728025e6b8d3b3367f6ac7
zedaenious <matt@zelenak.me>
2025-06-24 09:45:45 -0400

    add ordered paths for users me/id

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ app = FastAPI()
 async def root():
   return {"message": "This is my first FastAPI"}
 
+@app.get("/items/{num}")
+async def read_num(num: int):
+  return {"number": num}
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
   return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_current_user():
+  return {"user": "this is the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+  return {"user_id": user_id}
