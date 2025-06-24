2e25dee462355398ebf408ccb32c7692531cf99b
zedaenious <matt@zelenak.me>
2025-06-24 13:44:34 -0400

    add int type to PUT route item_id param

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def create_item(item: Item):
     return item_dict
   
 @app.put("/items/{item_id}")
-async def create_item(item_id, item: Item):
+async def create_item(item_id: int, item: Item):
   return {"item_id": item_id, **item.model_dump()}
 
 @app.get("/items/{num}")
