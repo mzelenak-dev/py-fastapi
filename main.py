49fc7de5855d91afb0cbf220c7c84822714ee9b3
zedaenious <matt@zelenak.me>
2025-06-24 13:41:09 -0400

    /items/ POST: add tax to price if provided

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 class Item(BaseModel):
   name: str
-  description: Option[str] = None
+  description: Optional[str] = None
   price: float
   tax: Optional[float] = None
 
@@ -17,7 +17,13 @@ async def root():
 
 @app.post("/items/")
 async def create_item(item: Item):
-  return item
+  item_dict = item.dict()
+  if item.tax:
+    price_with_tax = item.price + item.tax
+    item_dict.update({
+      "price_with_tax": price_with_tax
+    })
+    return item_dict
 
 @app.get("/items/{num}")
 async def read_num(num: int):
